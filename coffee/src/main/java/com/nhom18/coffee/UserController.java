e88e059d7be2cd5a5ee5d9e873a49cac93e924c2
tienkrp21-crypto <tienkrp21@gmail.com>
2026-03-14 19:07:56 +0700

    fix lan 3

--- coffee/src/main/java/com/nhom18/coffee/UserController.java
+++ coffee/src/main/java/com/nhom18/coffee/UserController.java
@@ -12,31 +12,10 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public List<User> getAll() {
-        return userRepository.findAll();
-    }
+    public List<User> getAll() { return userRepository.findAll(); }
 
     @GetMapping("/{id}")
     public User getOne(@PathVariable Integer id) {
         return userRepository.findById(id).orElse(null);
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
-    }
-
-    @PutMapping("/{id}")
-    public User updateUser(@PathVariable Integer id, @RequestBody User userDetails) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Not found: " + id));
-        user.setName(userDetails.getName());
-        return userRepository.save(user);
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable Integer id) {
-        userRepository.deleteById(id);
-        return "Deleted: " + id;
     }
 }
