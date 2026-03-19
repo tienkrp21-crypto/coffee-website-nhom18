77e7307aff7a4f9504731ecdcd55493815a31767
tienkrp21-crypto <tienkrp21@gmail.com>
2026-03-19 13:15:56 +0700

    fix lan 3

--- coffee/src/main/java/com/nhom18/coffee/UserController.java
+++ coffee/src/main/java/com/nhom18/coffee/UserController.java
@@ -8,14 +8,43 @@ import java.util.List;
 @RequestMapping("/users")
 @CrossOrigin("*")
 public class UserController {
+
     @Autowired
     private UserRepository userRepository;
 
+    // --- R: READ (Lấy toàn bộ danh sách User) ---
     @GetMapping
-    public List<User> getAll() { return userRepository.findAll(); }
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
 
+    // --- R: READ (Lấy thông tin User theo ID) ---
     @GetMapping("/{id}")
     public User getOne(@PathVariable Integer id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+    // --- C: CREATE (Thêm mới một User) ---
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // --- U: UPDATE (Cập nhật thông tin User theo ID) ---
+    @PutMapping("/{id}")
+    public User update(@PathVariable Integer id, @RequestBody User userDetails) {
+        return userRepository.findById(id).map(user -> {
+            // Cập nhật trường name dựa theo User.java bạn đã cung cấp
+            user.setName(userDetails.getName());
+            return userRepository.save(user);
+        }).orElse(null);
+    }
+
+    // --- D: DELETE (Xóa User theo ID) ---
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        if(userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+        }
     }
 }
