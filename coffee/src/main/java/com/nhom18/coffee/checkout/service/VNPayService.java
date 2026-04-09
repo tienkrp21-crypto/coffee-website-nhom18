7945eed61b9756303c9b90b1988ee175e64c8897
Bien <tranchanhbien@gmail.com>
2026-04-09 12:23:07 +0700

    update checkout

--- coffee/src/main/java/com/nhom18/coffee/checkout/service/VNPayService.java
+++ coffee/src/main/java/com/nhom18/coffee/checkout/service/VNPayService.java
@@ -53,8 +53,11 @@ public class VNPayService {
         vnp_Params.put("vnp_ReturnUrl", vnp_ReturnUrl);
         vnp_Params.put("vnp_IpAddr", ipAddress);
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        // Ép múi giờ VN cho Formatter
+        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh")); 
+
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
         
