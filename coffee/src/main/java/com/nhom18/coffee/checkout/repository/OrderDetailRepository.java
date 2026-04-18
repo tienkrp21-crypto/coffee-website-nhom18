3b13e5d0279424ed0582dc26446eaba77aafd874
tienkrp21-crypto <tienkrp21@gmail.com>
2026-04-18 11:10:39 +0700

    test

--- coffee/src/main/java/com/nhom18/coffee/checkout/repository/OrderDetailRepository.java
+++ coffee/src/main/java/com/nhom18/coffee/checkout/repository/OrderDetailRepository.java
@@ -1,6 +1,5 @@
 package com.nhom18.coffee.checkout.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,9 +7,4 @@ import com.nhom18.coffee.checkout.model.OrderDetail;
 
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
-    
-    // Hàm này dùng để lôi toàn bộ các món hàng thuộc về một đơn hàng cụ thể.
-    // Phục vụ cho chức năng Hoàn kho (Restore Stock) khi thanh toán thất bại hoặc hủy đơn.
-    List<OrderDetail> findByOrder_Id(Integer orderId); 
-    
 }
